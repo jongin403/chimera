eaef849754728e51bbf2f28c8b70f2e8ab1742f6
jongin403 <jongin403@naver.com>
2023-12-26 00:47:40 +0900

    feat: jest 설치 방법 변경 (Next 공식문서)

--- src/utils/number-format/number-format.test.ts
+++ src/utils/number-format/number-format.test.ts
@@ -1,20 +1,6 @@
 import { getCurrency, addComma } from './number-format';
 import { Locales } from '../../types/locale';
 
-describe('getCurrency', () => {
-  it('should return formatted date based on locale', () => {
-    const locale: Locales = 'en-US';
-    const format = 'short';
-    const expectedDate = new Date().toLocaleDateString(locale, {
-      dateStyle: format,
-    });
-
-    const result = getCurrency(locale, format);
-
-    expect(result).toBe(expectedDate);
-  });
-});
-
 describe('addComma', () => {
   it('should return number with comma separator', () => {
     const number = 1000000;
