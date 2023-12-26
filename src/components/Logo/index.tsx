fc2c495aab09d6397753e06ee1b46df792e374c7
jongin403 <jongin403@naver.com>
2023-12-27 00:49:46 +0900

    feat: logo 및 font 추가

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,6 +1,8 @@
 import { Chimera } from '@icons/Chimera';
 import styles from './Logo.module.scss';
 import classNames from 'classnames/bind';
+import Typo from '../atom/Typo';
+import { Noto_Sans_KR, Montserrat } from 'next/font/google';
 
 const cx = classNames.bind(styles);
 
@@ -8,10 +10,21 @@ export type LogoProps = {
   className?: string;
 };
 
+const notoSansKr = Noto_Sans_KR({
+  weight: ['700'],
+  subsets: ['latin'],
+});
+
+const montserrat = Montserrat({
+  weight: ['700'],
+  subsets: ['latin'],
+});
+
 const Logo = ({ className }: LogoProps) => {
   return (
     <div className={cx(className, styles.logoWrap)}>
-      <Chimera></Chimera>로고
+      <Chimera width={36} height={36}></Chimera>
+      <Typo className={cx(styles.logoName, notoSansKr.className)}>CHIMERA</Typo>
     </div>
   );
 };
