9288f7594d3bdecfb53f6335c7932fca330d259f
jongin403 <jongin403@naver.com>
2024-01-01 02:25:59 +0900

    storybook alias 경로 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,8 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -14,6 +17,24 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+        '@api': [path.resolve(__dirname, '../src/api')],
+        '@assets': [path.resolve(__dirname, '../src/assets')],
+        '@components': [path.resolve(__dirname, '../src/components')],
+        '@hooks': [path.resolve(__dirname, '../src/hooks')],
+        '@icons': [path.resolve(__dirname, '../src/icons')],
+        '@styles': [path.resolve(__dirname, '../src/styles')],
+        '@types': [path.resolve(__dirname, '../src/types')],
+        '@utils': [path.resolve(__dirname, '../src/utils')],
+        '@wrappers': [path.resolve(__dirname, '../src/wrappers')],
+      };
+    }
+    return config;
   },
 };
 export default config;
